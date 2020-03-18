be0ebb2196020758fc7efc3d3d9cfdd01a2c8d0c
Stasextaz <58384280+stasextaz@users.noreply.github.com>
2020-03-18 17:53:50 +0300

    Add files via upload

---
+++ lift.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lift : MonoBehaviour {
+    public float speed = 0;
+    public Vector3 direction;
+
+    public bool active = true;
+
+    void OnTriggerEnter(Collider other)
+    {
+       if (other.tag == "Stop")
+        {
+            direction = direction * -1;
+        }
+        if (other.tag == "Player")
+        {
+            active = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        active = false;
+    }
+    void Update () {
+		if (active)
+        {
+            transform.position += direction * speed * Time.deltaTime;
+        }
+	}
+}
