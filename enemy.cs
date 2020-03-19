628ba485165670c037ddc7741d73b6a9c607e6fc
Stasextaz <58384280+stasextaz@users.noreply.github.com>
2020-03-19 18:34:34 +0300

    Add files via upload

---
+++ enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class enemy : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        Player player = other.GetComponent<Player>();
+        if (player != null)
+        {
+            player.takeDamage();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
